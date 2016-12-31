47d0401d60680e663b39682819048b938ad176ed
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2016-12-31 07:10:09 -0800

    Add keyboard shortcuts for 'Open' and 'New'.

--- editor.js
+++ editor.js
@@ -7,22 +7,22 @@ function errorHandler(e) {
   var msg = "";
 
   switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
+  case FileError.QUOTA_EXCEEDED_ERR:
     msg = "QUOTA_EXCEEDED_ERR";
     break;
-    case FileError.NOT_FOUND_ERR:
+  case FileError.NOT_FOUND_ERR:
     msg = "NOT_FOUND_ERR";
     break;
-    case FileError.SECURITY_ERR:
+  case FileError.SECURITY_ERR:
     msg = "SECURITY_ERR";
     break;
-    case FileError.INVALID_MODIFICATION_ERR:
+  case FileError.INVALID_MODIFICATION_ERR:
     msg = "INVALID_MODIFICATION_ERR";
     break;
-    case FileError.INVALID_STATE_ERR:
+  case FileError.INVALID_STATE_ERR:
     msg = "INVALID_STATE_ERR";
     break;
-    default:
+  default:
     msg = "Unknown Error";
     break;
   };
@@ -38,7 +38,10 @@ function handleDocumentChange(title) {
     document.getElementById("title").innerHTML = title;
     document.title = title;
     if (title.match(/.json$/)) {
-      mode = {name: "javascript", json: true};
+      mode = {
+        name: "javascript",
+        json: true
+      };
       modeName = "JavaScript (JSON)";
     } else if (title.match(/.html$/)) {
       mode = "htmlmixed";
@@ -67,15 +70,15 @@ function setFile(theFileEntry, isWritable) {
 
 function readFileIntoEditor(theFileEntry) {
   if (theFileEntry) {
-    theFileEntry.file(function(file) {
+    theFileEntry.file(function (file) {
       var fileReader = new FileReader();
 
-      fileReader.onload = function(e) {
+      fileReader.onload = function (e) {
         handleDocumentChange(theFileEntry.fullPath);
         editor.setValue(e.target.result);
       };
 
-      fileReader.onerror = function(e) {
+      fileReader.onerror = function (e) {
         console.log("Read failed: " + e.toString());
       };
 
@@ -85,15 +88,15 @@ function readFileIntoEditor(theFileEntry) {
 }
 
 function writeEditorToFile(theFileEntry) {
-  theFileEntry.createWriter(function(fileWriter) {
-    fileWriter.onerror = function(e) {
+  theFileEntry.createWriter(function (fileWriter) {
+    fileWriter.onerror = function (e) {
       console.log("Write failed: " + e.toString());
     };
 
     var blob = new Blob([editor.getValue()]);
     fileWriter.truncate(blob.size);
-    fileWriter.onwriteend = function() {
-      fileWriter.onwriteend = function(e) {
+    fileWriter.onwriteend = function () {
+      fileWriter.onwriteend = function (e) {
         handleDocumentChange(theFileEntry.fullPath);
         console.log("Write completed.");
       };
@@ -103,17 +106,17 @@ function writeEditorToFile(theFileEntry) {
   }, errorHandler);
 }
 
-var onChosenFileToOpen = function(theFileEntry) {
+var onChosenFileToOpen = function (theFileEntry) {
   setFile(theFileEntry, false);
   readFileIntoEditor(theFileEntry);
 };
 
-var onWritableFileToOpen = function(theFileEntry) {
+var onWritableFileToOpen = function (theFileEntry) {
   setFile(theFileEntry, true);
   readFileIntoEditor(theFileEntry);
 };
 
-var onChosenFileToSave = function(theFileEntry) {
+var onChosenFileToSave = function (theFileEntry) {
   setFile(theFileEntry, true);
   writeEditorToFile(theFileEntry);
 };
@@ -124,25 +127,33 @@ function handleNewButton() {
     editor.setValue("");
   } else {
     chrome.app.window.create('main.html', {
-      frame: 'chrome', bounds: { width: 720, height: 400}
+      frame: 'chrome',
+      bounds: {
+        width: 720,
+        height: 400
+      }
     });
   }
 }
 
 function handleOpenButton() {
-  chrome.fileSystem.chooseEntry({ type: 'openWritableFile' }, onWritableFileToOpen);
+  chrome.fileSystem.chooseEntry({
+    type: 'openWritableFile'
+  }, onWritableFileToOpen);
 }
 
 function handleSaveButton() {
   if (fileEntry && hasWriteAccess) {
     writeEditorToFile(fileEntry);
   } else {
-    chrome.fileSystem.chooseEntry({ type: 'saveFile' }, onChosenFileToSave);
+    chrome.fileSystem.chooseEntry({
+      type: 'saveFile'
+    }, onChosenFileToSave);
   }
 }
 
 function initContextMenu() {
-  chrome.contextMenus.removeAll(function() {
+  chrome.contextMenus.removeAll(function () {
     for (var snippetName in SNIPPETS) {
       chrome.contextMenus.create({
         title: snippetName,
@@ -153,7 +164,7 @@ function initContextMenu() {
   });
 }
 
-chrome.contextMenus.onClicked.addListener(function(info) {
+chrome.contextMenus.onClicked.addListener(function (info) {
   // Context menu command wasn't meant for us.
   if (!document.hasFocus()) {
     return;
@@ -162,7 +173,7 @@ chrome.contextMenus.onClicked.addListener(function(info) {
   editor.replaceSelection(SNIPPETS[info.menuItemId]);
 });
 
-onload = function() {
+onload = function () {
   initContextMenu();
 
   newButton = document.getElementById("new");
@@ -174,15 +185,33 @@ onload = function() {
   saveButton.addEventListener("click", handleSaveButton);
 
   editor = CodeMirror(
-    document.getElementById("editor"),
-    {
-      mode: {name: "javascript", json: true },
+    document.getElementById("editor"), {
+      mode: {
+        name: "javascript",
+        json: true
+      },
       lineNumbers: true,
-      theme: "twilight",
+      theme: "zenburn",
       fixedGutter: true,
       extraKeys: {
-        "Cmd-S": function(instance) { handleSaveButton() },
-        "Ctrl-S": function(instance) { handleSaveButton() },
+        "Cmd-S": function (instance) {
+          handleSaveButton()
+        },
+        "Ctrl-S": function (instance) {
+          handleSaveButton()
+        },
+        "Cmd-O": function (instance) {
+          handleOpenButton()
+        },
+        "Ctrl-O": function (instance) {
+          handleOpenButton()
+        },
+        "Cmd-N": function (instance) {
+          handleNewButton()
+        },
+        "Ctrl-N": function (instance) {
+          handleNewButton()
+        },
       }
     });
 
@@ -190,7 +219,7 @@ onload = function() {
   onresize();
 };
 
-onresize = function() {
+onresize = function () {
   var container = document.getElementById('editor');
   var containerWidth = container.offsetWidth;
   var containerHeight = container.offsetHeight;
