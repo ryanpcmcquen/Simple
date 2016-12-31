1401b1725530bf03e9e7e2bd695e0ca07760872f
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2016-12-31 07:15:26 -0800

    Remove bounds.

--- background.js
+++ background.js
@@ -1,23 +1,23 @@
-chrome.app.runtime.onLaunched.addListener(function() {
+chrome.app.runtime.onLaunched.addListener(function () {
   // width 640 for font size 12
   //       720 for font size 14
   chrome.app.window.create('main.html', {
-    frame: 'chrome', bounds: { width: 720, height: 400}, minWidth:720, minHeight: 400
+    frame: 'chrome'
   });
 });
 
-/** 
-* Set up the Commands listener event
-* @see http://developer.chrome.com/trunk/apps/commands.html
-*/
+/**
+ * Set up the Commands listener event
+ * @see http://developer.chrome.com/trunk/apps/commands.html
+ */
 if (chrome.commands) {
-  chrome.commands.onCommand.addListener(function(command) {
-     console.log("Command triggered: " + command);
+  chrome.commands.onCommand.addListener(function (command) {
+    console.log("Command triggered: " + command);
 
-     if (command == "cmdNew") {
-       chrome.app.window.create('main.html', {
-         frame: 'chrome', bounds: { width: 720, height: 400}, minWidth:720, minHeight: 400
-       });
-     }
+    if (command == "cmdNew") {
+      chrome.app.window.create('main.html', {
+        frame: 'chrome'
+      });
+    }
   });
 }
