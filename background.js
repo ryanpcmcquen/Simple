b24b2a169a81b095f871e8f13b87d0c3f4006601
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2016-12-31 07:42:10 -0800

    Sync local versions.

--- background.js
+++ background.js
@@ -2,22 +2,17 @@ chrome.app.runtime.onLaunched.addListener(function () {
   // width 640 for font size 12
   //       720 for font size 14
   chrome.app.window.create('main.html', {
-    frame: 'chrome'
+    frame: 'chrome',
+    bounds: {
+      width: 640,
+      height: 400
+    },
   });
-});
-
-/**
- * Set up the Commands listener event
- * @see http://developer.chrome.com/trunk/apps/commands.html
- */
-if (chrome.commands) {
   chrome.commands.onCommand.addListener(function (command) {
-    console.log("Command triggered: " + command);
-
     if (command == "cmdNew") {
       chrome.app.window.create('main.html', {
         frame: 'chrome'
       });
     }
   });
-}
+});
