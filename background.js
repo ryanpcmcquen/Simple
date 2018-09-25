ae61d29dbde39f64c6c5ee6598eb85ecf6f8b726
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-09-25 06:49:30 -0700

    Working on saving theme preference.
    Signed-off-by: Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>

--- background.js
+++ background.js
@@ -1,11 +1,19 @@
 /* global chrome */
 chrome.app.runtime.onLaunched.addListener((launchData) => {
-    window.launchData = launchData
+console.log(chrome.storage.local.get())
+    window.launchData = Object.assign(
+        {},
+        launchData,
+        
+    )
     chrome.app.window.create('main.html', {
         frame: 'chrome',
         bounds: {
             width: 750,
-            height: 700
+            height: 600
         }
     })
+})
+chrome.runtime.onMessage.addListener((request) => {
+    navigator.webkitPersistentStorage.simplePreferences = request
 })
--- simple.js
+++ simple.js
@@ -7,264 +7,275 @@ FileError,
 odis
 */
 window.addEventListener('load', () => {
-    let fileEntry
-    let hasWriteAccess
-    const editor = ace.edit('editor')
+    chrome.runtime.getBackgroundPage((backgroundWindow) => {
+        console.log(backgroundWindow)
+        //const simplePreferences = backgroundWindow.launchData.simplePreferences
+        //const defaultTheme = simplePreferences ? simplePreferences.theme : 'twilight'
+        let fileEntry
+        let hasWriteAccess
+        const editor = ace.edit('editor')
 
-    const errorHandler = (e) => {
-        let msg = ''
+        const errorHandler = (e) => {
+            let msg = ''
 
-        switch (e.code) {
-            case FileError.QUOTA_EXCEEDED_ERR:
-                msg = 'QUOTA_EXCEEDED_ERR'
-                break
-            case FileError.NOT_FOUND_ERR:
-                msg = 'NOT_FOUND_ERR'
-                break
-            case FileError.SECURITY_ERR:
-                msg = 'SECURITY_ERR'
-                break
-            case FileError.INVALID_MODIFICATION_ERR:
-                msg = 'INVALID_MODIFICATION_ERR'
-                break
-            case FileError.INVALID_STATE_ERR:
-                msg = 'INVALID_STATE_ERR'
-                break
-            default:
-                msg = 'Unknown Error'
-                break
+            switch (e.code) {
+                case FileError.QUOTA_EXCEEDED_ERR:
+                    msg = 'QUOTA_EXCEEDED_ERR'
+                    break
+                case FileError.NOT_FOUND_ERR:
+                    msg = 'NOT_FOUND_ERR'
+                    break
+                case FileError.SECURITY_ERR:
+                    msg = 'SECURITY_ERR'
+                    break
+                case FileError.INVALID_MODIFICATION_ERR:
+                    msg = 'INVALID_MODIFICATION_ERR'
+                    break
+                case FileError.INVALID_STATE_ERR:
+                    msg = 'INVALID_STATE_ERR'
+                    break
+                default:
+                    msg = 'Unknown Error'
+                    break
+            }
+            throw new Error('Error: ' + msg)
         }
-        console.log('Error: ' + msg)
-    }
 
-    const modeList = ace.require('ace/ext/modelist')
-    const titleNode = document.getElementById('title')
-    // const modeNode = document.getElementById("mode")
-    const handleDocumentChange = (title) => {
-        let mode
-        if (title) {
-            title = title.match(/[^/]+$/)[0]
-            titleNode.textContent = title
-            document.title = title
-            mode = modeList.getModeForPath(title).mode
-        } else {
-            titleNode.textContent = '[No document loaded.]'
+        const modeList = ace.require('ace/ext/modelist')
+        const titleNode = document.getElementById('title')
+        // const modeNode = document.getElementById("mode")
+        const handleDocumentChange = (title) => {
+            let mode
+            if (title) {
+                title = title.match(/[^/]+$/)[0]
+                titleNode.textContent = title
+                document.title = title
+                mode = modeList.getModeForPath(title).mode
+            } else {
+                titleNode.textContent = '[No document loaded.]'
+            }
+            editor.session.setMode(mode)
+            if (/makefile/.test(mode)) {
+                editor.setOptions({
+                    tabSize: 8,
+                    useSoftTabs: false
+                })
+            } else {
+                editor.setOptions({
+                    tabSize: 4,
+                    useSoftTabs: true
+                })
+            }
+            // modeNode.textContent = mode.replace(/.*\//g, "")
         }
-        editor.session.setMode(mode)
-        if (/makefile/.test(mode)) {
-            editor.setOptions({
-                tabSize: 8,
-                useSoftTabs: false
-            })
-        } else {
-            editor.setOptions({
-                tabSize: 4,
-                useSoftTabs: true
-            })
+
+        const setFile = (theFileEntry, isWritable) => {
+            fileEntry = theFileEntry
+            hasWriteAccess = isWritable
         }
-        // modeNode.textContent = mode.replace(/.*\//g, "")
-    }
 
-    const setFile = (theFileEntry, isWritable) => {
-        fileEntry = theFileEntry
-        hasWriteAccess = isWritable
-    }
+        const readFileIntoEditor = (theFileEntry) => {
+            if (theFileEntry) {
+                theFileEntry.file((file) => {
+                    const fileReader = new FileReader()
 
-    const readFileIntoEditor = (theFileEntry) => {
-        if (theFileEntry) {
-            theFileEntry.file((file) => {
-                const fileReader = new FileReader()
+                    fileReader.onload = (e) => {
+                        handleDocumentChange(theFileEntry.fullPath)
+                        editor.setValue(e.target.result)
+                    }
 
-                fileReader.onload = (e) => {
-                    handleDocumentChange(theFileEntry.fullPath)
-                    editor.setValue(e.target.result)
+                    fileReader.onerror = (e) => {
+                        throw new Error('Read failed: ' + e.toString())
+                    }
+
+                    fileReader.readAsText(file)
+                }, errorHandler)
+            }
+        }
+
+        const writeEditorToFile = (theFileEntry) => {
+            theFileEntry.createWriter((fileWriter) => {
+                fileWriter.onerror = (e) => {
+                    throw new Error('Write failed: ' + e.toString())
                 }
 
-                fileReader.onerror = (e) => {
-                    console.log('Read failed: ' + e.toString())
-                }
+                const blob = new Blob([editor.getValue()])
+                fileWriter.truncate(blob.size)
+                fileWriter.onwriteend = () => {
+                    fileWriter.onwriteend = (e) => {
+                        handleDocumentChange(theFileEntry.fullPath)
+                    }
 
-                fileReader.readAsText(file)
+                    fileWriter.write(blob)
+                }
             }, errorHandler)
         }
-    }
 
-    const writeEditorToFile = (theFileEntry) => {
-        theFileEntry.createWriter((fileWriter) => {
-            fileWriter.onerror = (e) => {
-                console.log('Write failed: ' + e.toString())
-            }
+        const onWritableFileToOpen = (theFileEntry) => {
+            setFile(theFileEntry, true)
+            readFileIntoEditor(theFileEntry)
+        }
 
-            const blob = new Blob([editor.getValue()])
-            fileWriter.truncate(blob.size)
-            fileWriter.onwriteend = () => {
-                fileWriter.onwriteend = (e) => {
-                    handleDocumentChange(theFileEntry.fullPath)
+        const onChosenFileToSave = (theFileEntry) => {
+            setFile(theFileEntry, true)
+            writeEditorToFile(theFileEntry)
+        }
+
+        const handleNewButton = () => {
+            chrome.app.window.create('main.html', {
+                frame: 'chrome',
+                bounds: {
+                    width: 750,
+                    height: 700
                 }
+            })
+        }
 
-                fileWriter.write(blob)
-            }
-        }, errorHandler)
-    }
-
-    const onWritableFileToOpen = (theFileEntry) => {
-        setFile(theFileEntry, true)
-        readFileIntoEditor(theFileEntry)
-    }
-
-    const onChosenFileToSave = (theFileEntry) => {
-        setFile(theFileEntry, true)
-        writeEditorToFile(theFileEntry)
-    }
-
-    const handleNewButton = () => {
-        chrome.app.window.create('main.html', {
-            frame: 'chrome',
-            bounds: {
-                width: 750,
-                height: 700
-            }
-        })
-    }
-
-    const handleOpenButton = () => {
-        chrome.fileSystem.chooseEntry(
-            {
-                type: 'openWritableFile'
-            },
-            onWritableFileToOpen
-        )
-    }
-
-    const handleSaveButton = () => {
-        if (fileEntry && hasWriteAccess) {
-            writeEditorToFile(fileEntry)
-        } else {
+        const handleOpenButton = () => {
             chrome.fileSystem.chooseEntry(
                 {
-                    type: 'saveFile'
+                    type: 'openWritableFile'
                 },
-                onChosenFileToSave
+                onWritableFileToOpen
             )
         }
-    }
 
-    // Fix an odd scrolling error:
-    editor.$blockScrolling = Infinity
-    // Disable syntax checking:
-    editor.session.setOption('useWorker', false)
-
-    editor.commands.addCommands([
-        {
-            name: 'newFile',
-            bindKey: {
-                win: 'Ctrl-N',
-                mac: 'Command-N'
-            },
-            exec: handleNewButton
-        },
-        {
-            name: 'openFile',
-            bindKey: {
-                win: 'Ctrl-O',
-                mac: 'Command-O'
-            },
-            exec: handleOpenButton
-        },
-        {
-            name: 'saveFile',
-            bindKey: {
-                win: 'Ctrl-S',
-                mac: 'Command-S'
-            },
-            exec: handleSaveButton
-        },
-        {
-            name: 'increaseFontSize',
-            bindKey: {
-                win: 'Ctrl-=|Ctrl-+',
-                mac: 'Command-=|Command-+'
-            },
-            exec: () => {
-                editor.setOption('fontSize', editor.getOption('fontSize') + 2)
-            }
-        },
-        {
-            name: 'decreaseFontSize',
-            bindKey: {
-                win: 'Ctrl--|Ctrl-_',
-                mac: 'Command--|Command-_'
-            },
-            exec: () => {
-                editor.setOption('fontSize', editor.getOption('fontSize') - 2)
-            }
-        },
-        {
-            name: 'resetFontSize',
-            bindKey: {
-                win: 'Ctrl-0|Ctrl-Numpad0',
-                mac: 'Command-0|Command-Numpad0'
-            },
-            exec: () => {
-                editor.setOption('fontSize', 14)
+        const handleSaveButton = () => {
+            if (fileEntry && hasWriteAccess) {
+                writeEditorToFile(fileEntry)
+            } else {
+                chrome.fileSystem.chooseEntry(
+                    {
+                        type: 'saveFile'
+                    },
+                    onChosenFileToSave
+                )
             }
         }
-    ])
 
-    // Autosave:
-    editor.on(
-        'input',
-        odis.debounce(() => {
-            if (titleNode.textContent) {
-                handleSaveButton()
-            }
-        })
-    )
+        // Fix an odd scrolling error:
+        editor.$blockScrolling = Infinity
+        // Disable syntax checking:
+        editor.session.setOption('useWorker', false)
 
-    // Change themes:
-    document
-        .querySelector('.theme-chooser')
-        .addEventListener('click', (event) => {
-            if (/INPUT/.test(event.target.tagName)) {
-                editor.setTheme(`ace/theme/${event.target.value}`)
-            }
-        })
-
-    // Enable autocomplete:
-    ace.require('ace/ext/language_tools')
-    editor.setOptions({
-        tabSize: 4,
-        useSoftTabs: true,
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true
-    })
-    editor.setTheme('ace/theme/twilight')
-
-    chrome.runtime.getBackgroundPage((backgroundWindow) => {
-        if (
-            backgroundWindow.launchData &&
-            backgroundWindow.launchData.items &&
-            backgroundWindow.launchData.items.length
-        ) {
-            chrome.app.window.create(
-                'main.html',
-                {
-                    frame: 'chrome',
-                    bounds: {
-                        width: 750,
-                        height: 700
-                    }
+        editor.commands.addCommands([
+            {
+                name: 'newFile',
+                bindKey: {
+                    win: 'Ctrl-N',
+                    mac: 'Command-N'
                 },
-                () => {
-                    onWritableFileToOpen(
-                        backgroundWindow.launchData.items.pop().entry
+                exec: handleNewButton
+            },
+            {
+                name: 'openFile',
+                bindKey: {
+                    win: 'Ctrl-O',
+                    mac: 'Command-O'
+                },
+                exec: handleOpenButton
+            },
+            {
+                name: 'saveFile',
+                bindKey: {
+                    win: 'Ctrl-S',
+                    mac: 'Command-S'
+                },
+                exec: handleSaveButton
+            },
+            {
+                name: 'increaseFontSize',
+                bindKey: {
+                    win: 'Ctrl-=|Ctrl-+',
+                    mac: 'Command-=|Command-+'
+                },
+                exec: () => {
+                    editor.setOption('fontSize', editor.getOption('fontSize') + 2)
+                }
+            },
+            {
+                name: 'decreaseFontSize',
+                bindKey: {
+                    win: 'Ctrl--|Ctrl-_',
+                    mac: 'Command--|Command-_'
+                },
+                exec: () => {
+                    editor.setOption('fontSize', editor.getOption('fontSize') - 2)
+                }
+            },
+            {
+                name: 'resetFontSize',
+                bindKey: {
+                    win: 'Ctrl-0|Ctrl-Numpad0',
+                    mac: 'Command-0|Command-Numpad0'
+                },
+                exec: () => {
+                    editor.setOption('fontSize', 14)
+                }
+            }
+        ])
+
+        // Autosave:
+        editor.on(
+            'input',
+            odis.debounce(() => {
+                if (titleNode.textContent) {
+                    handleSaveButton()
+                }
+            })
+        )
+
+        // Change themes:
+        document
+            .querySelector('.theme-chooser')
+            .addEventListener('click', (event) => {
+                if (/INPUT/.test(event.target.tagName)) {
+                    editor.setTheme(`ace/theme/${event.target.value}`)
+                    chrome.storage.local.set(
+                        'simplePreferences',
+                        {
+                            theme: event.target.value
+                        }
                     )
                 }
-            )
-        } else {
-            return false
-        }
-    })
+            })
 
-    editor.focus()
+        // Enable autocomplete:
+        ace.require('ace/ext/language_tools')
+        editor.setOptions({
+            tabSize: 4,
+            useSoftTabs: true,
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true
+        })
+        editor.setTheme(`ace/theme/${defaultTheme}`)
+
+        chrome.runtime.getBackgroundPage((backgroundWindow) => {
+            if (
+                backgroundWindow.launchData &&
+                backgroundWindow.launchData.items &&
+                backgroundWindow.launchData.items.length
+            ) {
+                chrome.app.window.create(
+                    'main.html',
+                    {
+                        frame: 'chrome',
+                        bounds: {
+                            width: 750,
+                            height: 700
+                        }
+                    },
+                    () => {
+                        onWritableFileToOpen(
+                            backgroundWindow.launchData.items.pop().entry
+                        )
+                    }
+                )
+            } else {
+                return false
+            }
+        })
+
+        editor.focus()
+    })
 })
